81fa8fec3e5d841ac153e91fe9cd2f2c84d58103
ljpassingby <740461957@qq.com>
2018-05-07 20:37:09 +0800

    a tmall test

--- src/test/TestJava.java
+++ src/test/TestJava.java
@@ -4,6 +4,6 @@ public class TestJava {
     public static void main(String[] args){
         char[] cs = {'2','a',' '};
         System.out.println(new String(cs));
-        System.out.println("123");
+        System.out.println("345");
     }
 }
