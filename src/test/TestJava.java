8ff16df6945f4c02930740e2a887e4880f4365c1
ljpassingby <740461957@qq.com>
2018-05-10 21:11:55 +0800

    新增了filter和CategoryServlet基本代码以及其父类BaseBackServlet的反射的代码

--- src/test/TestJava.java
+++ src/test/TestJava.java
@@ -2,7 +2,7 @@ package test;
 
 public class TestJava {
     public static void main(String[] args){
-        char[] cs = {'2','a',' '};
+        char[] cs = {'2','a',' ','3'};
         System.out.println(new String(cs));
     }
 }
