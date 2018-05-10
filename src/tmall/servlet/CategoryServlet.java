252a8c539f229f217bd28fd9dbd130244490563c
ljpassingby <740461957@qq.com>
2018-05-10 21:04:03 +0800

    新增了filter和CategoryServlet基本代码以及其父类BaseBackServlet的反射的代码

---
+++ src/tmall/servlet/CategoryServlet.java
@@ -0,0 +1,33 @@
+package tmall.servlet;
+
+import tmall.util.Page;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CategoryServlet extends BaseBackServlet {
+    @Override
+    public String add(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+    @Override
+    public String delete(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+    @Override
+    public String update(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+    @Override
+    public String get(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+
+    @Override
+    public String list(HttpServletRequest request, HttpServletResponse response, Page page) {
+        return null;
+    }
+}
